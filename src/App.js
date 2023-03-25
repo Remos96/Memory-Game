38abcce2ea0d4ccfae3d93721e6589f30e9c208d
Remos96 <omer122@protonmail.com>
2023-03-25 03:11:40 -0400

    game is working roughly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import CardGrid from "./Components/CardGrid";
 
+function newGame() {
+  window.location.reload(false);
+}
+
 function App() {
   return (
     <div className="App">
       <h1>he</h1>
+      <button onClick={newGame}> New Game </button>
       <CardGrid />
     </div>
   );
--- src/Components/CardGrid.js
+++ src/Components/CardGrid.js
@@ -1,33 +1,83 @@
 import { useState } from "react";
 import Card from "./Card";
-import Bear from "../assets/bear1.jpg";
-import Cat1 from "../assets/cat1.jpg";
-import Cat2 from "../assets/cat2.jpg";
-import Chicken1 from "../assets/chicken1.jpg";
-import Dino1 from "../assets/dino1.jpg";
-import Fox1 from "../assets/fox1.jpg";
-import Fox2 from "../assets/fox2.jpg";
-import Panda1 from "../assets/panda1.jpg";
-
 
 function CardGrid() {
-  const [images, setImages] = useState([
-    Bear,
-    Cat1,
-    Cat2,
-    Chicken1,
-    Dino1,
-    Fox1,
-    Fox2,
-    Panda1,
-  ]);
-  const imageGrid = [...images, ...images].sort(() => Math.random() - 0.5);
-  console.log(Bear);
+  let complete = 0;
+  const [turns, setTurns] = useState(0);
+  const [latestItem, setLatestItem] = useState(-999); // id needs to be < 0 as our image indexs in the grid are 0 -> 15
+  const [images, setImages] = useState(
+    [
+      { id: 100, image: "bear1.jpg", cardState: "" },
+      { id: 100, image: "bear1.jpg", cardState: "" },
+      { id: 101, image: "cat1.jpg", cardState: "" },
+      { id: 101, image: "cat1.jpg", cardState: "" },
+      { id: 102, image: "cat2.jpg", cardState: "" },
+      { id: 102, image: "cat2.jpg", cardState: "" },
+      { id: 103, image: "chicken1.jpg", cardState: "" },
+      { id: 103, image: "chicken1.jpg", cardState: "" },
+      { id: 104, image: "dino1.jpg", cardState: "" },
+      { id: 104, image: "dino1.jpg", cardState: "" },
+      { id: 105, image: "fox1.jpg", cardState: "" },
+      { id: 105, image: "fox1.jpg", cardState: "" },
+      { id: 106, image: "fox2.jpg", cardState: "" },
+      { id: 106, image: "fox2.jpg", cardState: "" },
+      { id: 107, image: "panda1.jpg", cardState: "" },
+      { id: 107, image: "panda1.jpg", cardState: "" },
+    ].sort(() => Math.random() - 0.5)
+  );
+  // does not work, creates undefined half way thru grid -> const imageGrid = [...images, ...images].sort(() => Math.random() - 0.5);
+
+  // In order to re-render the page and element w/ update without needing a page refresh, use setState
+  // https://stackoverflow.com/questions/51426496/why-we-cant-change-states-in-react-without-calling-setstate
+  function checkIfCorrect(currentCard) {
+    if (images[latestItem].id === images[currentCard].id) {
+      images[latestItem].cardState = "matched";
+      images[currentCard].cardState = "matched";
+      // complete = complete + 1;
+      setImages([...images]);
+      setLatestItem(-999);
+      setTurns(turns + 1);
+      // console.log(complete);
+      if (complete === 8){
+        setTurns(turns + "complete!");
+      }
+    } else {
+      images[currentCard].cardState = "incorrectpair";
+      images[latestItem].cardState = "incorrectpair";
+      setImages([...images]);
+      setTimeout(() => {
+        images[currentCard].cardState = "";
+        images[latestItem].cardState = "";
+        setImages([...images]);
+      }, 666);
+      setLatestItem(-999);
+      setTurns(turns + 1);
+    }
+  }
+
+  function onClickCheck(currentCard) {
+    if (latestItem === -999) {
+      // see if we have already clicked on a image
+
+      images[currentCard].cardState = "checking";
+      setImages([...images]);
+      setLatestItem(currentCard);
+    } else {
+      checkIfCorrect(currentCard);
+    }
+  }
+
   return (
     <div className="game-area">
-      {imageGrid.map((image) => (
-        <Card src={image} />
+      {images.map((image, index) => (
+        <Card
+          key={index} // W/o key, program fails to run properly
+          image={image}
+          index={index}
+          onClickCheck={onClickCheck}
+        />
       ))}
+      <div className="Turn">Total Turns: {turns}</div>
     </div>
   );
 }
