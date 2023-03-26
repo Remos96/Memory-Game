7741cbdf1f8a04ac011ab33802e4d078fc263f48
Remos96 <omer122@protonmail.com>
2023-03-26 18:24:02 -0400

    Final version of product

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import "./App.css";
 import CardGrid from "./Components/CardGrid";
 
-// function newGame() {
-//   window.location.reload(false);
-// }
-
 function App() {
   return (
     <div className="App">
       <h1> Memory Game </h1>
-      {/* <button onClick={newGame}> Reload Page! </button> */}
       <CardGrid />
     </div>
   );
