c9a5a2dfda525e6ea54f4a32d389f8e6972c651e
Remos96 <omer122@protonmail.com>
2023-03-26 17:20:42 -0400

    Add 'grid-complete' alert

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,7 +1,9 @@
 export default function Card({ image, index, onClickCheck }) {
-
   return (
-    <div className={"card" + image.cardState} onClick={() => onClickCheck(index)}>
+    <div
+      className={"card" + image.cardState}
+      onClick={() => onClickCheck(index)}
+    >
       <img src={image.image} />
     </div>
   );
